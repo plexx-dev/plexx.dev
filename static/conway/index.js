cd55c021cbed65acb7e09c80227d3f72dbca5f6b
plexx-dev <plexx@plexx.dev>
2025-05-17 09:37:34 +0200

    renamed libs for better readability

--- static/conway/index.js
+++ static/conway/index.js
@@ -1,10 +1,10 @@
 // Import our outputted wasm ES6 module
 // Which, export default's, an initialization function
-//import greeter from "/static/game-of-life/pkg/wasm_game_of_life.js";
+//import greeter from "/static/conwy/pkg/wasm_game_of_life.js";
 //
 //greeter("moin");
 
-import { greeter } from '/static/game-of-life/pkg/wasm_game_of_life.js3'
+import { greeter } from '/static/conwy/pkg/wasm_game_of_life.js3'
 
 let greeting = greeter('Grafbase')
 
