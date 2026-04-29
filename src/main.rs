209c970f430821d458f30cb14b37667739f596b1
plexx-dev <plexx@plexx.dev>
2026-04-29 21:42:46 +0200

    added iam a Teapot

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use std::path::Path;
 
-use rocket::{catch, catchers, get, launch, routes, Request};
-use rocket::shield::Shield;
 use rocket::fs::{FileServer, NamedFile};
 use rocket::http::Status;
+use rocket::shield::Shield;
+use rocket::{Request, catch, catchers, get, launch, routes};
 
 use rocket_dyn_templates::{Template, context};
-mod error;
 mod conway;
+mod error;
 mod games;
-mod snake;
 mod ping_pong;
+mod snake;
 
 #[get("/")]
 async fn index() -> Template {
@@ -22,14 +22,27 @@ async fn robots() -> Option<NamedFile> {
     NamedFile::open(Path::new("static/robots.txt")).await.ok()
 }
 
-#[catch(default)]
-async fn default_catcher(status: Status, _: &Request<'_>) -> Template {
-    Template::render("error", context! {
-        code: status.code,
-        message: error::get_error_message(status.code)
-    })
+#[get("/teapot")]
+async fn teapot() -> Template {
+    Template::render(
+        "error",
+        context! {
+            code: 418,
+            message: error::get_error_message(418)
+        },
+    )
 }
 
+#[catch(default)]
+async fn default_catcher(status: Status, _: &Request<'_>) -> Template {
+    Template::render(
+        "error",
+        context! {
+            code: status.code,
+            message: error::get_error_message(status.code)
+        },
+    )
+}
 
 #[launch]
 async fn rocket() -> _ {
@@ -37,24 +50,11 @@ async fn rocket() -> _ {
 
     rocket::build()
         .attach(shield)
-        .mount("/", routes![
-            index
-        ])
-        .mount("/", routes![
-            robots
-        ])
-        .mount("/games/", routes![
-            games::index,
-        ])
-        .mount("/games/snake", routes![
-            snake::routes::index,
-        ])
-        .mount("/games/conway", routes![
-            conway::routes::index,
-        ])
-        .mount("/games/ping_pong", routes![
-            ping_pong::routes::index,
-        ])
+        .mount("/", routes![index, robots, teapot])
+        .mount("/games/", routes![games::index,])
+        .mount("/games/snake", routes![snake::routes::index,])
+        .mount("/games/conway", routes![conway::routes::index,])
+        .mount("/games/ping_pong", routes![ping_pong::routes::index,])
         .mount("/static", FileServer::from("static"))
         .mount("/game_files", FileServer::from("game_files"))
         .register("/", catchers![default_catcher])
